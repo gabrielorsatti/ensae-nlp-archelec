36fac7268715727be8f93a01c7133f0e0ac69b11
gabrielorsatti <gabriel.orsatti@ensae.fr>
2026-03-13 13:41:37 +0000

    importation

--- notebooks/download_data.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-# Télécharger les métadonnées directement depuis le site Archelec
-url_metadata = "https://archelec.sciencespo.fr/export/csv"  
-metadata = pd.read_csv(url_metadata, sep=';')
-metadata.to_csv('../data/archelec_metadata.csv', index=False)
